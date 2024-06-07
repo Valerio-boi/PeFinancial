ec1797a36c06fb422bfe9a9a5df958d3ef279d89
Valerio boi <boivalerio@gmail.com>
2024-06-07 15:34:29 +0200

    Inizio progetto swift per gestione finanze

---
+++ PeFinancial/UICommon/UIExtensions.swift
@@ -0,0 +1,18 @@
+//
+//  UIExtensions.swift
+//  PeFinancial
+//
+//  Created by Valerio boi on 07/06/24.
+//
+
+import SwiftUI
+
+struct UIExtensions: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UIExtensions()
+}
